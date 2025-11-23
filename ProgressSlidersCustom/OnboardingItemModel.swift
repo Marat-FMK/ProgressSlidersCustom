5409ef96513f93244b7b2d7fe38f1b76ab7a245e
marat_FMK <kosjachok8@yandex.ru>
2025-11-23 23:36:27 +0300

    example items for test

--- ProgressSlidersCustom/OnboardingItemModel.swift
+++ ProgressSlidersCustom/OnboardingItemModel.swift
@@ -12,4 +12,10 @@ struct OnboardingItem: Identifiable {
     let title: String
     let description: String
     let image: String
+    
+    static let example = [OnboardingItem(title: "hello", description: " приветствую нового юзера", image: "hello"),
+                   OnboardingItem(title: "navigation", description: "у нас классная навигация", image: "navigation"),
+                   OnboardingItem(title: "security", description: "мы думаем о твоей безопасности", image: "security"),
+                   OnboardingItem(title: "search", description: "написали поиск для запросов", image: "search"),
+    ]
 }
