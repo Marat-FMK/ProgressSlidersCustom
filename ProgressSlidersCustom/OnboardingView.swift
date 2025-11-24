11a680402e8fadb800f924e169d6f8442a2d73ca
marat_FMK <kosjachok8@yandex.ru>
2025-11-24 18:32:30 +0300

    onboardingView complete

--- ProgressSlidersCustom/OnboardingView.swift
+++ ProgressSlidersCustom/OnboardingView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
+import Combine
 
 struct OnboardingView: View {
+    @StateObject var viewModel = OnboardingViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                HStack {
+                    ProgressView()
+                }
+                .onAppear {
+                    viewModel.startProgress()
+                }
+                VStack(alignment: .leading) {
+                    Text(viewModel.screenInfo[viewModel.currentSlide]["title"] ?? "")
+                        .font(.title)
+                    
+                    Text(viewModel.screenInfo[viewModel.currentSlide]["text"] ?? "")
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                
+                Image(systemName: "x.mark")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                
+                Spacer()
+                
+                Button {
+                    viewModel.goNext()
+                } label: {
+                    Text("next")
+                }
+            }
+            .padding()
+    }
+}
+
+extension OnboardingView {
+    var progressView: some View {
+        
+        ForEach(viewModel.screenInfo.indices, id: \.self) { slide in
+            GeometryReader { proxy in
+                ZStack(alignment: .leading) {
+                    Capsule()
+                        .fill(.gray)
+                        .frame(height: 5)
+                    if slide < viewModel.currentSlide {
+                        Capsule()
+                            .fill(.black)
+                            .frame(height: 5)
+                            .frame(width: proxy.size.width)
+                    } else if slide == viewModel.currentSlide {
+                        Capsule()
+                            .fill(.black)
+                            .frame(height: 5)
+                            .frame(width: proxy.size.width * self.viewModel.progress, alignment: .leading)
+                    }
+                }
+                .onTapGesture {
+                    viewModel.currentSlide = slide
+                    viewModel.startProgress()
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
