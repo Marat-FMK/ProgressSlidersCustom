b3b0c1171023532944bf9e59186bf0a5bea42add
marat_FMK <kosjachok8@yandex.ru>
2025-11-25 19:50:04 +0300

    ProgressSlidersView rewrite to swift file

--- ProgressSlidersCustom/ProgressSliders.swift
+++ ProgressSlidersCustom/ProgressSliders.swift
@@ -5,15 +5,46 @@
 //  Created by Marat Fakhrizhanov on 24.11.2025.
 //
 
-import Foundation
 import SwiftUI
 
-//struct ProgressSliders: View {
-////    @Binding var curentSlide: Int
-//    let items: [OnboardingItem]
-//    
-//    var body: some View {
-//        
-//        
-//    }
-//}
+struct ProgressSlidersView: View {
+    
+    @Binding var progress: CGFloat
+    @Binding var currentSlide: Int
+    let items: [OnboardingItem]
+    
+    let startProgress: () -> Void
+    
+    var body: some View {
+        
+        HStack {
+            ForEach(items.indices, id: \.self) { slide in
+                GeometryReader { proxy in
+                    ZStack(alignment: .leading) {
+                        Capsule()
+                            .fill(.gray)
+                        if slide < currentSlide {
+                            Capsule()
+                                .fill(.black)
+                                .frame(width: proxy.size.width)
+                        } else if slide == currentSlide {
+                            Capsule()
+                                .fill(.black)
+                                .frame(width: proxy.size.width * self.progress, alignment: .leading)
+                        }
+                    }
+                    .onTapGesture {
+                        currentSlide = slide
+                        startProgress()
+                    }
+                }
+            }
+        }
+        .frame(height: 5)
+        .padding(.horizontal, 20)
+        .onAppear {
+            startProgress()
+        }
+        
+    }
+}
